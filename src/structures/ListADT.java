b748f3205ebadfd707b31cce7c0d0c7e30020cd6
MyeongchanJin <jmcchan@naver.com>
2020-09-27 15:03:06 +0900

    리스트 공통 메서드의 인터페이스 ListADT 생성

---
+++ src/structures/ListADT.java
@@ -0,0 +1,24 @@
+package structures;
+
+//********************************************************************
+//  ListADT.java       Authors: Lewis/Chase
+//
+//  Defines the interface to a general list collection. Specific
+//  types of lists will extend this interface to complete the
+//  set of necessary operations.
+//********************************************************************
+
+import java.util.Iterator;
+
+public interface ListADT<T> {
+    public T removeFirst();
+    public T removeLasts();
+    public T remove(T element);
+    public T first();
+    public T last();
+    public boolean contains(T target);
+    public boolean isEmpty();
+    public int size();
+    public Iterator<T> iterator();
+    public String toString();
+}
