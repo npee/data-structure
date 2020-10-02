1bcb50bfa8e16508d713f957dd0c0cb711ab99f5
MyeongchanJin <jmcchan@naver.com>
2020-10-03 00:00:35 +0900

    Binary search 메서드, 정렬 조건용 Selection sort 메서드 작성, 테스트

--- src/_11_SortingAndSearching/SortingAndSearching.java
+++ src/_11_SortingAndSearching/SortingAndSearching.java
@@ -2,6 +2,27 @@ package _11_SortingAndSearching;
 
 public class SortingAndSearching<T extends Comparable> {
 
+    // Sorting
+    public void selectionSort(T[] data) {
+        int min;
+        T temp;
+        for (int index = 0; index < data.length - 1; index++) {
+            min = index;
+            for (int scan = index + 1; scan < data.length; scan++) {
+                if (data[scan].compareTo(data[min]) < 0) {
+                    min = scan;
+                }
+            }
+
+            temp = data[min];
+            data[min] = data[index];
+            data[index] = temp;
+        }
+    }
+
+
+
+    // Searching
     public boolean linearSearch(T[] data, int min, int max, T target) {
         int index = min;
         boolean found = false;
@@ -11,6 +32,23 @@ public class SortingAndSearching<T extends Comparable> {
                 found = true;
             }
             index++;
+        }
+
+        return found;
+    }
+
+    public boolean binarySearch(T[] data, int min, int max, T target) {
+        boolean found = false;
+        int mid = (min + max) / 2;
+
+        if (data[mid].compareTo(target) == 0) {
+            found = true;
+        } else if (data[mid].compareTo(target) > 0) {
+            if (min <= mid - 1) {
+                found = binarySearch(data, min, mid, target);
+            } else if (mid + 1 <= max) {
+                found = binarySearch(data, mid + 1, max, target);
+            }
         }
 
         return found;
--- src/_11_SortingAndSearching/test.java
+++ src/_11_SortingAndSearching/test.java
@@ -7,5 +7,19 @@ public class test {
         SortingAndSearching<Integer> sorts = new SortingAndSearching<>();
 
         System.out.println(sorts.linearSearch(x, 0, 5, 3));
+
+        sorts.selectionSort(x);
+        printArray(x);
+
+        System.out.println(sorts.binarySearch(x, 0, 5, 3));
+
+    }
+
+    public static <T> void printArray(T[] array) {
+        StringBuilder s = new StringBuilder();
+        for (int scan = 0; scan < array.length; scan++) {
+            s.append(array[scan]).append(" ");
+        }
+        System.out.println(s);
     }
 }
